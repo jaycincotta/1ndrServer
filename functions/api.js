aa2824d19dc9f5ba9da7cd770d1e9ec9921af719
Jay Cincotta <jay.cincotta@gmail.com>
2022-08-15 01:34:00 -0700

    Refined cancelUrl behavior

--- functions/api.js
+++ functions/api.js
@@ -26,67 +26,67 @@ router.get('/hello', (req, res) => {
 })
 
 router.post("/checkout", async (req, res) => {
-	console.log("REQUEST BODY", req.body, req.headers.referer);
-	const { title, product, price, format } = req.body
+	console.log("REQUEST BODY", req.body);
+	const { title, product, price, format, cancelUrl } = req.body
 	const successURL = req.headers.referer + "thanks"
-	const cancelURL = req.headers.referer + "error"
+	const cancelURL = cancelUrl
 	const errorURL = req.headers.referer + "error"
 
 
 	try {
-	const session = await stripe.checkout.sessions.create({
-		mode: 'payment',
-		payment_method_types: ['card'],
-		success_url: successURL,
-		cancel_url: cancelURL,
-		automatic_tax: { enabled: false },
-		shipping_address_collection: {
-			allowed_countries: ['US'],
-		},
-		phone_number_collection: {
-			enabled: true,
-		},
-		shipping_options: [
-			{
-				shipping_rate_data: {
-					type: 'fixed_amount',
-					fixed_amount: {
-						amount: 0,
-						currency: 'usd',
-					},
-					display_name: 'Free shipping',
-					delivery_estimate: {
-						minimum: {
-							unit: 'business_day',
-							value: 5,
-						},
-						maximum: {
-							unit: 'business_day',
-							value: 10,
-						},
-					}
-				}
+		const session = await stripe.checkout.sessions.create({
+			mode: 'payment',
+			payment_method_types: ['card'],
+			success_url: successURL,
+			cancel_url: cancelURL,
+			automatic_tax: { enabled: false },
+			shipping_address_collection: {
+				allowed_countries: ['US'],
 			},
-
-		],
-		line_items: [{
-			price_data: {
-				unit_amount: price * 100,
-				currency: "USD",
-				product_data: {
-					name: `${title}: ${product}`
-				},
-			},
-			adjustable_quantity: format === "original" ? { enabled: false } : {
+			phone_number_collection: {
 				enabled: true,
-				minimum: 1,
-				maximum: 10
 			},
-			quantity: 1
-		}]
-	})
-	console.log("STRIPE RESPONSE", session.url, session)
-	res.json({ url: session.url })
+			shipping_options: [
+				{
+					shipping_rate_data: {
+						type: 'fixed_amount',
+						fixed_amount: {
+							amount: 0,
+							currency: 'usd',
+						},
+						display_name: 'Free shipping',
+						delivery_estimate: {
+							minimum: {
+								unit: 'business_day',
+								value: 5,
+							},
+							maximum: {
+								unit: 'business_day',
+								value: 10,
+							},
+						}
+					}
+				},
+
+			],
+			line_items: [{
+				price_data: {
+					unit_amount: price * 100,
+					currency: "USD",
+					product_data: {
+						name: `${title}: ${product}`
+					},
+				},
+				adjustable_quantity: format === "original" ? { enabled: false } : {
+					enabled: true,
+					minimum: 1,
+					maximum: 10
+				},
+				quantity: 1
+			}]
+		})
+		console.log("STRIPE RESPONSE", session.url, session)
+		res.json({ url: session.url })
 	} catch {
 		res.json({ url: errorURL })
 	}
